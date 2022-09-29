3c938e4a982fa3f69f9bf6bbebf3e20d701d4950
Katine <31282487+Kaizening@users.noreply.github.com>
2022-09-28 23:44:23 -0300

    Update recipePicker.py
    Hasta 38:50

--- recipePicker.py
+++ recipePicker.py
@@ -31,16 +31,41 @@ def fetch_db():
     table_name = all_tables[idx][1]
     # Una vez seleccionado el nombre de la tabla, podemos hacer un fetch de todos sus ingredientes
     cursor.execute("SELECT * FROM " + table_name + ";")
-    # Guardamos todos los ingredientes seleccionados en ingredients
-    ingredients = cursor.fetchall()
-    # Imprimimos los ingredientes
-    print(ingredients)
-    print(table_name)
-    
+    # Guardamos todos los ingredientes seleccionados en table_records
+    # Guardar los registros/filas de la tabla
+    table_records = cursor.fetchall()    
     # Debemos terminar la conexion que hemos establecido
     connection.close()
+    
+    return table_name, table_records
+
+def pre_process(table_name, table_records):
+    # Quitar los numero al final del titulo, selecciona todo hasta 6 caracteres antes
+    title = table_name[:-6]
+    # Agregar espacios entre cada palabra, agregando un espacio antes de una mayuscula
+    # Si el caracter es minuscula lo deja como esta, sino concatena un espacio
+    # Usar el metodo join para concatenar todos los valores de una lista en un string
+    title = "".join([char if char.islower() else " " + char for char in title])
+    
+    # Crear una lista vacia llamada ingredients
+    ingredients = []
+    
+    # Ingredients
+    # Repasar los ingredientes/filas de una tabla, en este caso una variable guarda
+    # todos los registros de la tabla
+    for i in table_records:
+        name = i[1]
+        qty = i[2]
+        unit = i[3]
+        ingredients.append(qty + " " + unit + " of " + name)
+        # "2 cups of sugar"
+    
+    return title, ingredients
+    
 
 def load_frame1():
+    # Este metodo apila un frame sobre otro. El mas relevante va arriba
+    frame1.tkraise()   
     # Forma de prevenir que un widget child modifique el parent con el method propagate
     frame1.pack_propagate(False)    
     # frame1 widgets
@@ -65,7 +90,6 @@ def load_frame1():
         ).pack()
 
     # Button widget
-
     tk.Button(
         frame1,
         text="SHUFFLE0",
@@ -79,8 +103,56 @@ def load_frame1():
         ).pack(pady=20)
 
 def load_frame2():
-    fetch_db()
-
+    # Para poder moverse entre frames, debemos iniciar cada frame con un .tkraise
+    # Este metodo apila un frame sobre otro. El mas relevante va arriba
+    frame2.tkraise()    
+    # Recibimos de la funcion fetch_db() table_name y table_records
+    table_name, table_records = fetch_db()
+    title, ingredients = pre_process(table_name, table_records)
+    
+    # frame1 widgets
+    logo_img = ImageTk.PhotoImage(file="assets/RRecipe_logo_bottom.png")
+    # Una vez que tenemos cargada la imagen, podemos convertirla en un widget
+    # tkinter no tiene un widget especifico para imagenes por lo que se usa el tipo Label
+    # fundamental indicar que es image=logo_img
+    logo_widget = tk.Label(frame2, image=logo_img, bg=bg_colour)
+    # Estamos obligados a acceder al atributo image, de nuestro logo_widget 
+    # y volver a setearlo como logo_img
+    logo_widget.image = logo_img
+    logo_widget.pack(pady=20)
+    
+    # Label widget
+    tk.Label(
+        frame2,
+        text=title,
+        bg=bg_colour,
+        fg="white",
+        font=("TkHeadingFont", 20)
+        ).pack(pady=25)
+    
+    for i in ingredients:
+        # Para cada ingrediente se creara su propio label
+        # Label widget
+        tk.Label(
+            frame2,
+            text=i,
+            bg=bg_colour,
+            fg="white",
+            font=("TkMenuFont", 12)
+            ).pack()
+        
+    # Button widget
+    tk.Button(
+        frame2,
+        text="BACK",
+        font=("TkHeadingFont", 18),
+        bg="#28393a",
+        fg="white",
+        cursor="hand2",
+        activebackground="#badee2",
+        activeforeground="black",
+        command= lambda:load_frame1()# Es una funcion que se ejecuta cada vez que se presiona el boton. Este caso es frame1 para volver atras
+        ).pack(pady=20)
 
 # Initiallize app
 root = tk.Tk()
